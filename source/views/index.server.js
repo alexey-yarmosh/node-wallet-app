28f1bfcadf5d2094695142b8936b97540a5bb3ce
Baderfall <delete-863@yandex.by>
2017-10-20 15:55:40 +0000

    server webpack-bundle ready

---
+++ source/views/index.server.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { App } from './components';
+
+module.exports = () => {
+  return <App />;
+};
--- webpack.config.js
+++ webpack.config.js
@@ -2,44 +2,71 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const fs = require('fs');
 
-// function getExternals() {
-//   return fs.readdirSync('node_modules')
-//     .concat(['react-dom/server'])
-//     .filter((module) => module !== '.bin')
-//     .reduce((externals, module) => {
-//       externals[module] = `commonjs ${module}`;
-//       return externals;
-//     }, {});
-// }
+function getExternals() {
+  return fs.readdirSync('node_modules')
+    .concat(['react-dom/server'])
+    .filter((module) => module !== '.bin')
+    .reduce((externals, module) => {
+      externals[module] = `commonjs ${module}`;
+      return externals;
+    }, {});
+}
 
-module.exports = {
-  entry: './source/views/index.js',
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader'
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
-        })
-      }
+module.exports = [
+  {
+    name: 'client',
+    entry: './source/views/index.client.js',
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: 'bundle.client.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        // {
+        //   test: /\.js$/,
+        //   exclude: /node_modules/,
+        //   loader: 'eslint-loader'
+        // },
+        {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: 'css-loader'
+          })
+        }
+      ]
+    },
+    plugins: [
+      new ExtractTextPlugin('styles.css')
     ]
   },
-  plugins: [
-    new ExtractTextPlugin('styles.css')
-  ]
-};
+  {
+    name: 'server',
+    entry: './source/views/index.server.js',
+    output: {
+      path: path.resolve(__dirname, 'source/views'),
+      filename: 'bundle.server.js',
+      libraryTarget: 'umd'
+    },
+    target: 'node',
+    externals: getExternals(),
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.css$/,
+          loader: 'ignore-loader'
+        }
+      ]
+    }
+  }
+];
