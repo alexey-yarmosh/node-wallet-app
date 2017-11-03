0d0accc40979bee9e141f7d1b5fecd54bda84e40
Aliaksei Yarmash [EPAM] <Aliaksei_Yarmash@epam.com>
2017-11-03 18:28:13 +0300

    mobilePay.js refactor

--- source/controllers/actions/mobilePay.js
+++ source/controllers/actions/mobilePay.js
@@ -1,18 +1,18 @@
 module.exports = async ctx => {
-    const id = Number(ctx.params.id);
-    const sum = Number(ctx.request.body.amount);
+    const cardId = parseInt(ctx.params.id, 10);
+    const { amount, phoneNumber } = ctx.request.body;
 
-    await ctx.cardsModel.pay(id, sum);
+    await ctx.cardsModel.pay(cardId, parseInt(amount, 10));
 
-    const transaction = {
+    const newTransaction = await ctx.transactionsModel.add({
         type: 'paymentMobile',
-        data: '+7(921)3333333',
-        cardId: id,
-        sum,
-        cardNumber: '546925000000000'
-    };
+        data: {
+            phoneNumber
+        },
+        cardId,
+        sum: parseInt(amount, 10)
+    });
 
-    const newTransaction = await ctx.transactionsModel.add(transaction);
     ctx.status = 201;
     ctx.body = newTransaction;
 };
