7aac40e4d152d0e0dac26169963e3fea234e3b9f
Aliaksei Yarmash [EPAM] <Aliaksei_Yarmash@epam.com>
2017-11-03 18:36:02 +0300

    mobilePay comission added

--- source/controllers/actions/mobilePay.js
+++ source/controllers/actions/mobilePay.js
@@ -1,8 +1,11 @@
+const COMISSION = 3;
+
 module.exports = async ctx => {
     const cardId = parseInt(ctx.params.id, 10);
     const { amount, phoneNumber } = ctx.request.body;
+    const resultAmount = parseInt(amount, 10) + COMISSION;
 
-    await ctx.cardsModel.pay(cardId, parseInt(amount, 10));
+    await ctx.cardsModel.pay(cardId, resultAmount);
 
     const newTransaction = await ctx.transactionsModel.add({
         type: 'paymentMobile',
@@ -10,7 +13,7 @@ module.exports = async ctx => {
             phoneNumber
         },
         cardId,
-        sum: parseInt(amount, 10)
+        sum: resultAmount
     });
 
     ctx.status = 201;
